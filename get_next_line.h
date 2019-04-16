a45ed0b34c7f47a88f7dfa6213890e527e9ab070
Francisco Guzman <fguzman@e1z3r1p17.42.us.org>
2019-04-16 15:59:00 -0700

    getnextline

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fguzman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/26 15:37:42 by fguzman           #+#    #+#             */
+/*   Updated: 2019/04/16 14:00:50 by fguzman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 50
+
+int get_next_line(const int fd, char **line);
+
+#endif
